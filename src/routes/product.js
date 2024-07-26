3d49c0aa10efef3f0e7680900028069fd3fdacb6
Akmal Irfansyahir <128498082+Affnsyhr@users.noreply.github.com>
2024-07-26 20:27:53 +0700

    Update product.js

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/product');
-const { authenticateToken,  authorizeRole} = require('../middlewares/auth');
+const express = require('express'); // Mengimpor modul express untuk membuat router
+const router = express.Router(); // Membuat instance router dari express
+const productController = require('../controllers/product'); // Mengimpor controller untuk produk
+const { authenticateToken, authorizeRole } = require('../middlewares/auth'); // Mengimpor middleware untuk otentikasi dan otorisasi
 
-router.get('/', authenticateToken, authorizeRole(['admin_gudang', 'manager']),productController.getAllProducts);
-router.post('/', authenticateToken, authorizeRole(['admin_gudang', 'manager']),productController.createProduct);
+// Route untuk mendapatkan semua produk
+// Middleware authenticateToken memverifikasi token JWT
+// Middleware authorizeRole memastikan hanya 'admin_gudang' dan 'manager' yang dapat mengakses
+router.get('/', authenticateToken, authorizeRole(['admin_gudang', 'manager']), productController.getAllProducts);
+
+// Route untuk membuat produk baru
+// Middleware authenticateToken memverifikasi token JWT
+// Middleware authorizeRole memastikan hanya 'admin_gudang' dan 'manager' yang dapat mengakses
+router.post('/', authenticateToken, authorizeRole(['admin_gudang', 'manager']), productController.createProduct);
+
+// Route untuk memperbarui produk berdasarkan ID
+// Middleware authenticateToken memverifikasi token JWT
+// Middleware authorizeRole memastikan hanya 'admin_gudang' dan 'manager' yang dapat mengakses
 router.put('/:id', authenticateToken, authorizeRole(['admin_gudang', 'manager']), productController.updateProduct);
-router.delete('/:id',authenticateToken, authorizeRole(['admin_gudang', 'manager']), productController.deleteProduct);
 
+// Route untuk menghapus produk berdasarkan ID
+// Middleware authenticateToken memverifikasi token JWT
+// Middleware authorizeRole memastikan hanya 'admin_gudang' dan 'manager' yang dapat mengakses
+router.delete('/:id', authenticateToken, authorizeRole(['admin_gudang', 'manager']), productController.deleteProduct);
+
+// Mengekspor router agar dapat digunakan di file lain
 module.exports = router;
