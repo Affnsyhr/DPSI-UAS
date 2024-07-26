96eab924fd0eb5e24a57706e27f9df9e767c8190
Akmal Irfansyahir <128498082+Affnsyhr@users.noreply.github.com>
2024-07-26 18:57:39 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,14 @@
 const { Sequelize } = require('sequelize');
-const config = require('../src/config/config'); [process.env.NODE_ENV || 'development'];
+require('dotenv').config();
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
 });
 
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
